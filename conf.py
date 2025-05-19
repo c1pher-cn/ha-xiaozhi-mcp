3e6f3e9b02400d6f7fa02f5b3737257af76a6876
c1pher <c1pher-cn@users.noreply.github.com>
2025-05-19 22:12:03 +0800

    Update conf.py
    MCP_ENDPOINT

--- conf.py
+++ conf.py
@@ -2,4 +2,4 @@ HASS_CONFIG = {
     "api_key": "yourtoken",
     "base_url": "https://192.168.2.3:8123"
 }
-MCP_URL = 'wss://api.xiaozhi.me/mcp/?token=xxxxxxx'
+MCP_ENDPOINT = 'wss://api.xiaozhi.me/mcp/?token=xxxxxxx'
